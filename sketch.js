2c3b1dc659b429daba2741d56bf0e8f75b4e53e0
Ahsoka23 <89179461+Ahsoka23@users.noreply.github.com>
2022-04-19 14:18:15 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+
+function setup(){
+  createCanvas(400,400)
+  
+  //for (/*instructions for how many times the loop is being repeated*/){
+    //result that the program shall be doing while the loop is working
+  //}
+ 
+  
+  //create a for loop that show on the console every even number from 0 to 100 
+
+}
+
+function draw(){
+  background("white")
+    drawSprites();
+  // for loop
+
+  //while loop
+
+  // do while 
+
+}
+
